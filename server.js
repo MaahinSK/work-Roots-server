5f997ffa7a0dedb090e9ed00f22aef098cd8fd71
MaahinSK <maahin810@gmail.com>
2025-11-27 08:22:25 +0600

    sds

--- server.js
+++ server.js
@@ -21,7 +21,13 @@ app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true }));
 
 // Enable CORS
-app.use(cors());
+app.use(cors({
+  origin: [
+    'http://localhost:3000',
+    'https://work-roots-server.vercel.app/'
+  ],
+  credentials: true
+}));
 
 // Mount routers
 app.use('/api/auth', auth);
